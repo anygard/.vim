1a11e8ff616cd2db297a7626c7e254da85fb9894
Anders Nygard <anygard@redbridge.se>
2014-03-22 20:26:07 +0100

    Beginnings of Windows support

---
+++ install.cmd
@@ -0,0 +1,16 @@
+REM Copyright 2011, 2012, 2013, 2014 Anders Nygård
+REM
+REM This program is free software: you can redistribute it and/or modify it
+REM under the terms of the GNU General Public License as published by the Free
+REM Software Foundation, either version 3 of the License, or (at your option)
+REM any later version.
+REM
+REM This program is distributed in the hope that it will be useful, but
+REM WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+REM or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+REM for more details.
+REM
+REM You should have received a copy of the GNU General Public License along
+REM with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+REM This is just a place holder for the time being
